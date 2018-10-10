c559a865310a13928771a037b32ae0831b188673
Steve Godin <steve.god@gmail.com>
2018-10-10 09:48:32 -0700

    Setup static assets middleware and serve image

--- js_express_middleware_and_cookies/index.js
+++ js_express_middleware_and_cookies/index.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
@@ -8,18 +9,41 @@ const bodyParser = require("body-parser");
 // `app` object
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: true }));
-
 // this registers EJS as our default `view engine` meaning that when you
 // attempt to render templates, it will look for a matching file with `.ejs` extension
 // with a folder named `views`
 app.set("view engine", "ejs");
 
+// MIDDLEWARE
+
+// LOGGER
 // when we call `app.use` we're registering a `middleware` that works well
 // with Express.js. The middleware can be a function we write ourselves, or it
 // can be a `plugin` (Node Module) like morgan which we installed using the `npm install`
 // command
 app.use(morgan("dev"));
+
+// BODY PARSER
+// When setting the option "extended" to `true`, express
+// will support arrays and objects as data in addition
+// to strings and numbers from forms.
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// STATIC ASSETS
+// Use `path.join` to combine strings into directory paths.
+// Example:
+// path.join("/", "Users", "bob") // "/Users/bob"
+
+// `__dirname` is a global variable available in Node
+// that returns the full directory path beginning from the
+// root (i.e. /) of your computer to the file where `__dirname`
+// is located.
+
+// The static assets middleware will take all files and directories
+// inside a specified path and serve them all as URLs to the web.
+// This allows to send images, css files, js files, videos, etc.
+console.log("__dirname in ./index.js:", __dirname);
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use((request, response, next) => {
   console.log(">>>>");
