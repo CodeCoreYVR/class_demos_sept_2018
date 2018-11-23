58c3e5f6bc11f0b68318904b2730ed964d7d0263
Steve Godin <steve.god@gmail.com>
2018-11-23 15:50:38 -0800

    Add webpack-serve for hot reloading

--- js_webpack_basics/webpack_demo/src/client.js
+++ js_webpack_basics/webpack_demo/src/client.js
@@ -11,5 +11,9 @@ document.addEventListener("DOMContentLoaded", () => {
   const mooseImg = document.createElement("img");
   mooseImg.src = moosePic;
 
+  const h1 = document.createElement("h1");
+  h1.innerText = "Webpack Demo";
+
   document.body.append(mooseImg);
+  document.body.prepend(h1);
 });
